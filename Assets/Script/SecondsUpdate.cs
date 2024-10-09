ff1569a034cd599963236d5990b1cc4a50cbf3c0
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-09 14:44:45 +0100

    Initial commit

---
+++ Assets/Script/SecondsUpdate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+    public float timeStartOffset = 0;
+    bool gotStartTime = false; 
+    void Update()
+    {
+        if (!gotStartTime)
+        {
+            timeStartOffset = Time.realtimeSinceStartup;
+            gotStartTime = true;
+        }
+        transform.position = new Vector3(transform.position.x,
+                                        transform.position.y,
+                                        Time.realtimeSinceStartup - timeStartOffset);
+    }
+}
